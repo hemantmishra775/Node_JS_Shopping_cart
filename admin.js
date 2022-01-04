71fc7d4baa98517b797ac517ba2574b29c431aab
hemantmishra775 <54470266+hemantmishra775@users.noreply.github.com>
2022-01-04 18:56:34 +0530

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add_product',(req,res,next)=>
+{
+    res.send(`<html><form action='/product' method="POST"><input type="text" name="title"><button type="submit">submit</button></form></html>`)
+})
+
+router.post('/product',(req,res,next)=>
+{   
+    console.log("html");
+    console.log(req.body);
+    res.redirect('/');
+})
+
+
+module.exports = router;
