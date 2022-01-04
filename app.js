d562e0b71e9c29338ba1aca4864681d06801f06a
hemantmishra775 <54470266+hemantmishra775@users.noreply.github.com>
2022-01-04 18:56:05 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const bodyparser = require('body-parser')
+
+const app = express();
+
+const adminroutes = require('./routes/admin');
+const shoproutes = require('./routes/shop');
+
+app.use(bodyparser.urlencoded({ extended: true }));
+
+
+app.use(adminroutes);
+app.use(shoproutes);
+
+
+const Server = http.createServer(app);
+
+Server.listen(3000,'localhost',function(){
+    const address = Server.address().address;
+    const port = Server.address().port;
+
+    console.log(`Server is listening at http://${address}:${port}`);
+})
